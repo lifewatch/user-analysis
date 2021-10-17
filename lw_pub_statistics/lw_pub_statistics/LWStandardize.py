283a88a8e6726efba7b649027738c95d46ec10ef
Laurian Van Maldeghem <laurian.van.maldeghem@vliz.be>
2021-10-17 13:01:01 +0200

    Restructure & add similarity match

--- lw_pub_statistics/lw_pub_statistics/LWStandardize.py
+++ lw_pub_statistics/lw_pub_statistics/LWStandardize.py
@@ -1,17 +1,15 @@
-#import statements
-import difflib
+# import statements
 import pandas as pd
 import numpy as np
 import os
-from difflib import SequenceMatcher
+import shutil
 from pandas._testing import assert_index_equal
 from pandas.api.types import is_string_dtype
 
+from .functions import load_folderdata, load_filedata, best_standmatch, update_datafile
+
 
 class Standardizator:
-    """
-    Documentation should capture what this class is about. Mention mainly its responsibility and collaboration with others classes.
-    """
 
     def __init__(self, pubfolder: str) -> pd.DataFrame:
         """
@@ -22,121 +20,137 @@ class Standardizator:
         :returns: dataframe object of publication files in folder
         :rtype: pd.DataFrame
         """
-       
-        dflist = []
-        for filename in os.listdir(pubfolder):
-            filedir = os.path.join(os.pardir, 'publication_data', filename)
-            if filename.endswith('.xlsx') or filename.endswith('.xls'):
-                dflist += [pd.read_excel(filedir)]
-            elif filename.endswith('.csv'):
-                dflist += [pd.read_csv(filedir)]
-        
-        # assert compatibility of dataframe headers
-        series0 = dflist[0].columns
-        for df in dflist:
-            assert_index_equal(series0, df.columns)
-            series0 = df.columns
-            
-        self.data = pd.concat(dflist)
-       
+
+        # load standardized data if present, otherwise load non stand data:
+        if os.path.isfile(os.path.join(os.pardir, 'standardized_data', 'stand_data.csv')):
+            self.data = load_filedata(os.path.join(
+                os.pardir, 'standardized_data', 'stand_data.csv'))
+        else:
+            self.data = load_folderdata(pubfolder, 'publication_data')
 
     def add_WOSaffil(self, wos_export: str) -> pd.DataFrame:
         """
-        This method adds affiliation information of the first author, exported from Web of Science, to a column 'wow_affil'    
-        Note: other columns could be added aswell
-
-        :param stand_file: folder with wos export files 
-        :type stand_file: str  
+        Add affiliation information of the first author, exported from Web of Science
+        :param stand_file: folder with wos export files
+        :type stand_file: str
         """
 
-        self.data["wos_affil"] = np.nan
-        for wos_file in os.listdir(wos_export):
-            # Read data
-            assert wos_file.endswith('.csv'), "Please make sure the wos_export is stored as a csv file"
-            wos_file_dir = os.path.join(os.pardir,"wos_export", wos_file)
-            export_data = pd.read_csv(wos_file_dir)            
-            
-            # 1. wos affiliation of first author | 'RP' = first author name & affiliation info ; 'RP_2' = first author affiliation info
-            affil_firstauth = []
-            for index, row in export_data['RP'].iteritems():
-                affil_firstauth += [str(row).split('(corresponding author), ')[-1]]
-            export_data['RP_2'] = affil_firstauth
+        self.export_data = load_folderdata(wos_export, 'wos_export')
 
-            # 2. add wos_affil to data based on wos code | 'UT' = wos code
-            for index, row in export_data.iterrows():
-                self.data.loc[ self.data['WoScode'] == row['UT'], "wos_affil"] = row['RP_2']
-        
-        print("added wos affiliation to the pulication data")
+        if 'wos_affil' not in self.data.columns:
+            for index1, row in self.export_data.iterrows():
+                # note: type of nan values in export_data['RP'] is float, which is non iterable
+                if type(row['RP']) == str:
+                    # 1. wos affiliation of first author | 'RP' = first author name & affiliation info ; 'RP_2' = first author affiliation info
+                    affil_firstauth = str(row['RP']).split(
+                        '(corresponding author), ')[-1]
+                    # 2. add wos_affil to data based on wos code | 'UT' = wos code
+                    self.data.loc[self.data['WoScode'] == row['UT'],
+                                  "wos_affil"] = affil_firstauth
+
+            update_datafile(self.data)
+            print("WoS affiliations added to %s" % os.path.join(
+                os.pardir, 'standardized_data', 'stand_data.csv'))
+
+        return self
+
+    def add_WOScountry(self, wos_export: str) -> pd.DataFrame:
+        """
+        Add the country of the affiliation of the first author, exported from Web of Science
+        :param stand_file: folder with wos export files
+        :type stand_file: str
+        """
+
+        # Read data
+        self.export_data = load_folderdata(wos_export, 'wos_export')
+
+        if 'wos_country' not in self.data.columns:
+            # 1. wos affiliation of first author | 'RP' = first author name & affiliation info
+            for index, row in self.export_data.iterrows():
+                # get country name from string & clean up
+                country_firstauth = str(row['RP']).split(
+                    ', ')[-1]
+                country_firstauth = country_firstauth.rstrip(
+                    country_firstauth[-1])
+                if 'USA' in country_firstauth:
+                    country_firstauth = 'USA'
+
+                # 2. add wos_affil to data based on wos code | 'UT' = wos code
+                self.data.loc[self.data['WoScode'] == row['UT'],
+                              "wos_country"] = country_firstauth
+
+            # update datafile
+            update_datafile(self.data)
+            print("Country information added to %s" % os.path.join(
+                os.pardir, 'standardized_data', 'stand_data.csv'))
         return self
 
     def exactMatch(self, stand_file: str) -> pd.DataFrame:
         """
-        This method check for extact matches between standardized affiliation names and 'raw' affiliation names in the data (columns 'Affiliation' and 'wos_affil').
-        In case of an exact match, the standardized affiliation name is added in the 'stand_affil' column.
+        Check for extact matches between 'Affiliation' (from IMIS) and 'Institute' (from Affiliation_Standardized);
+        and adds 'Institute Standardized' to stand_affil in case of exact match.
+        :param stand_file: standardized data file
+        :type stand_file: str
+        """
+
+        # Load standardized affiliation names:
+        self.stand_data = load_filedata(stand_file)
+
+        # Find exact match & add standardized affiliation:
+        if 'stand_method' not in self.data.columns or 'exact match' not in self.data['stand_method']:
+            for index, row in self.stand_data.iterrows():
+                self.data.loc[self.data['Affiliation'] == row['Institute'],
+                              "stand_affil"] = row['Institute standardized']
+                self.data.loc[self.data['Affiliation'] == row['Institute'],
+                              "stand_method"] = 'exact match'
+
+            update_datafile(self.data)
+            print("Added exact matches between 'Affiliation' column and 'Institute' column from standardized list to %s" %
+                  os.path.join(os.pardir, 'standardized_data', 'stand_data.csv'))
+
+        return self
+
+    def similarityMatch(self, stand_file: str, col_stand: str) -> pd.DataFrame:
+        """
+        Check similarity between 'Institute standardized' and 'raw' affiliation names in the data (columns 'Affiliation' or 'wos_affil').
+        In case of a similarity higher than 80%, the standardized affiliation name is added in the 'stand_affil' column.
 
         :param stand_file: standardized data file
         :type stand_file: str
         """
 
-        ##TODO: FINISH!
-        # Standardized information as dataframe:
-        self.stand_data = pd.read_excel(stand_file, sheet_name='Affiliations_stand_LongList')
-        print('Checking for an exact match between Affiliation names and standardized names from list')
-        # Find exact matches & add standardized affiliation:
-        for index, row in self.stand_data.iterrows():
-            self.data.loc[ self.data['Affiliation'] == row['Institute'], ["stand_affil"]] = row['Institute standardized']
+        # Load standardized affiliation names:
+        self.stand_data = load_filedata(stand_file)
+
+        col_text = 'similarity match %s' % col_stand
+        if 'stand_method' not in self.data.columns or col_text not in self.data['stand_method']:
+            print(col_text)
+            """# select a subset of data that isn't standardized yet
+            if 'stand_affil' in self.data.columns:
+                self.nonstand_data = self.data.loc[
+                    self.data['stand_affil'] == 'nan']
+            else:
+                self.nonstand_data = self.data"""
+
+            # standardize data
+            for index1, row1 in self.data.iterrows():
+                stand_affil = row1['stand_affil']
+
+                # non stand data:
+                if type(stand_affil) == float:
+                    affiliation = str(row1[col_stand])
+
+                    best_match = best_standmatch(
+                        self.stand_data, affiliation)
+                    print(best_match)
+                    if best_match is not None:
+                        stand_affil = best_match
+
+                self.data.at[index1, 'stand_affil'] = stand_affil
+                self.data.at[index1, 'stand_method'] = col_text
+
+            update_datafile(self.data)
+            print("0.80 similar matches between %s column and 'Institute' column from standardized list were added to %s" % (
+                  col_stand, os.path.join(os.pardir, 'standardized_data', 'stand_data.csv')))
 
         return self
-
-
-    def similarityMatch(self, stand_file: str) -> pd.DataFrame:
-        """
-        This method checks the similarity between standardized affiliation names and 'raw' affiliation names in the data (columns 'Affiliation' and 'wos_affil').
-        In case of a similarity higher than ...%, the standardized affiliation name is added in the 'stand_affil' column.
-
-        :param stand_file: standardized data file
-        :type stand_file: str
-        """
-        
-        """
-        seq1 = self.data["Affiliation"]
-        seq2 = self.stand_data["Institute standardized"]
-        
-        test1 = difflib.SequenceMatcher
-        test1.set_seqs(seq1, seq2)
-        test1.ratio()
-        print(test1)
-        """
-        
-        #TODO FINISH!
-        
-        difference = difflib.Differ()
-
-        for index1, row1 in self.data.iterrows():
-            seq1 = row1["Affiliation"]
-
-            for index2, row2 in self.stand_data.iterrows():
-                seq2 = row2["Institute standardized"]
-                #print(seq1, seq2)
-
-        return self
-
-    def output(self, output_file: str) -> float:
-        """
-        This method writes the pd.Dataframe, with added standerdized information, to a new csv file
-
-        :param outputCSV: file name of the output csv that will be generated
-        :type outputCSV: str
-        """
-
-        if output_file.endswith('.xlsx')  or output_file.endswith('.xls'):
-            self.data.to_excel(output_file)
-            print("written to ", output_file)
-        elif output_file.endswith('.csv'):
-            self.data.to_csv(output_file)
-            print("Data was written to ", output_file)
-        else:
-            print("unsupported data format")
-
-        return self
-        
--- lw_pub_statistics/lw_pub_statistics/__main__.py
+++ lw_pub_statistics/lw_pub_statistics/__main__.py
@@ -4,6 +4,7 @@ import sys
 import logging
 
 from .LWStandardize import *
+from .LWlabels import *
 
 logname = 'lw_pub_statistics'
 logfile = logname + '.log'
@@ -15,25 +16,48 @@ def get_arg_parser():
     """
     Defines the arguments to this script by using Python's [argparse](https://docs.python.org/3/library/argparse.html)
     """
-    parser = argparse.ArgumentParser(description='Standardizes (lifewatch) publication data exported from IMIS',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-pubdata', '--publicationData', required=True, help='Specifies the folder with publication data')
-    parser.add_argument('-refdata', '--referenceData', help='Specifies the folder with standardized data')
-    parser.add_argument('-wosdata', '--wosExportData', help='Specifies the folder with export data from Web of Science (from publication with a WoS code)')
-    parser.add_argument('-out', '--output', help='Specifies the file with updated/standardized publication data')
+    parser = argparse.ArgumentParser(
+        description='Standardizes (lifewatch) publication data exported from IMIS',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '-pubdata',
+        '--publicationData',
+        required=True,
+        help='Specifies the folder with publication data')
+    parser.add_argument(
+        '-reffile',
+        '--referenceFile',
+        help='Specifies the file with standardized data')
+    parser.add_argument(
+        '-wosdata',
+        '--wosExportData',
+        help='Specifies the folder with export data from Web of Science (from publication with a WoS code)')
+    parser.add_argument(
+        '-col',
+        '--column',
+        choices=['Affiliation', 'wos_affil'],
+        help='Specifies the column used to match similarity against in the reference data')
 
     return parser
+
 
 def make_standardized(args: argparse.Namespace):
     df = Standardizator(args.publicationData)
     if args.wosExportData is not None:
         df.add_WOSaffil(args.wosExportData)
-    if args.referenceData is not None:
-        df.exactMatch(args.referenceData)
-        #df.similarityMatch(args.referenceData)
-    if args.output is not None:
-        df.output(args.output)
+        df.add_WOScountry(args.wosExportData)
+    if args.referenceFile is not None:
+        # df.exactMatch(args.referenceFile)
+        if args.column is not None:
+            df.similarityMatch(args.referenceFile, args.column)
+
     return df
+
+
+"""def add_labels(args: argparse.Namespace):
+    df = Labeller(args)  # to check if works!
+    return df"""
 
 
 def main():
@@ -41,8 +65,14 @@ def main():
     The main entry point to this module.
     """
     args = get_arg_parser().parse_args()
+    # standardize the data
     df = make_standardized(args)
+    # standardized_data = make_standardized(args)
+    # add labels to the standardized data
+    """df = add_labels(standardized_data)"""
+
     log.debug("df = %s" % df)
+
 
 if __name__ == '__main__':
     main()
